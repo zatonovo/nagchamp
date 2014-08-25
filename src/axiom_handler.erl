b22d534b9f745de9c4d6e0219f3ecd5822a04573
Isaul Vargas <ivargas@sailthru.com>
2014-08-25 19:35:02 -0400

    Adding to REST Api

--- src/axiom_handler.erl
+++ src/axiom_handler.erl
@@ -10,6 +10,14 @@ handle(<<"GET">>, [], Request) ->
   lager:info("[~p] Ping",[?MODULE]),
   "ok";
 
+handle(<<"GET">>, [<<"rest">>, <<"jobs">>], Request) ->
+  champ:list();
 
-handle(<<"GET">>, [<<"rest">>,<<"jobs">>], Request) ->
+handle(<<"GET">>, [<<"rest">>, <<"job">>, Url, Delay], Request) ->
+  StringUrl = binary_to_list(<<"http://", Url/binary>>),
+  champ:start_job(StringUrl, binary_to_integer(Delay)),
+  "ok";
+
+handle(<<"GET">>, [<<"rest">>, <<"job">>, JobId], Request) ->
+  champ:stop_job(JobId),
   "ok".
